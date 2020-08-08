a2180e260e74736969642b5201a6d270e69ca2cc
minsoo0715 <56436283+minsoo0715@users.noreply.github.com>
2020-08-08 22:33:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var router = require('./router/main')(app);
 app.set('views', __dirname + '/public');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
     console.log("Express server has started on port 3000");
+    console.log(process.env.PORT);
 });
 app.use(express.static('public'));
