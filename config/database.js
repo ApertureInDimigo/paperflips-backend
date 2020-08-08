b64347cd84ac64d0691f4aec34d7568fe5e3fdb0
minsoo0715 <56436283+minsoo0715@users.noreply.github.com>
2020-08-08 23:06:44 +0900

    ,

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: '7334',
-    database: 'paperflips'
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b5c72055d07cd3',
+    password: 'afca248a',
+    database: 'heroku_4ca6dadd6911bbc'
 };
