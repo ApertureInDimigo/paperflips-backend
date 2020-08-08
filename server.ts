5a8a7cb8c09583cc6e5b90c727113f50a2223a83
minsoo0715 <56436283+minsoo0715@users.noreply.github.com>
2020-08-08 22:14:50 +0900

    Update server.ts
    .

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ app.set('views', __dirname + '/public');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000 , function(){
     console.log("Express server has started on port 3000");
 })
 
