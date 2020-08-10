053babfeef6b7859365a5167e00df379364316e9
WhiteB <whiteblock0715@gmail.com>
2020-08-10 18:26:03 +0900

    encrypt, SQL Injection 방지 작업

---
+++ checker.ts
@@ -0,0 +1,23 @@
+const exp:RegExp = /^[0-9]+$/;
+const exp_id:RegExp = /^[A-Za-z0-9]{6,12}$/;         
+const exp_name:RegExp = /^[A-Z0-9a-z가-힣]+$/
+const exp_pwd:RegExp = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;  //8~15자리 
+
+
+export function check_id(object:string) {
+    return exp_id.test(object);
+}
+
+export function check_pwd(object:string) {
+    return exp_pwd.test(object);
+}
+
+
+export function check_name(object:string) {
+    return exp_name.test(object);
+}
+
+export function check(object:string) {
+    return exp.test(object);
+}
+
--- router/recipe.js
+++ router/recipe.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var mysql = require('mysql'); //mysql 모듈
 var dbconfig = require('../config/database.ts'); //database 구조
 var connection = mysql.createConnection(dbconfig); //mysql 연결
+var checker_1 = require("../checker");
 var util_1 = require("util");
 function checkconnect() {
     connection.on('error', function (err) {
@@ -18,27 +19,38 @@ function checkconnect() {
     });
 }
 router.get('/data/:seq', function (req, res) {
-    if (util_1.isUndefined(req.params.seq))
-        console.log('undefined');
-    checkconnect();
-    console.log('recipe get');
-    connection.query('SELECT recipeName,rarity,summary from Recipe WHERE seq=\'' + req.params.seq + '\'', function (error, rows) {
-        if (error) {
-            console.log(error);
-            // res.send(JSON.parse('{\"status\" : 404}'));
+    if (!checker_1.check(req.params.seq)) {
+        if (util_1.isUndefined(req.params.seq))
+            console.log('undefined');
+        checkconnect();
+        console.log('recipe get');
+        var seq = req.params.seq;
+        if (checker_1.check(seq.toString())) {
+            connection.query('SELECT recipeName,rarity,summary from Recipe WHERE seq=\'' + req.params.seq + '\'', function (error, rows) {
+                if (error) {
+                    console.log(error);
+                    // res.send(JSON.parse('{\"status\" : 404}'));
+                }
+                console.log('recipe info is: ', rows);
+                try {
+                    var obj = JSON.stringify(rows);
+                    // let obj2:any = JSON.parse( "{" + obj + "," + "\"status\": 200}");
+                    var obj2 = JSON.parse("{" + "\"data\":" + obj.substring(1, obj.length - 1) + "," + "\"status\": 200" + "}");
+                    res.send(obj2);
+                    //  res.send(obj2);
+                }
+                catch (e) {
+                    console.log(e);
+                    res.send(JSON.parse('{\"status\" : 404}'));
+                }
+            });
         }
-        console.log('recipe info is: ', rows);
-        try {
-            var obj = JSON.stringify(rows);
-            // let obj2:any = JSON.parse( "{" + obj + "," + "\"status\": 200}");
-            var obj2 = JSON.parse("{" + "\"data\":" + obj.substring(1, obj.length - 1) + "," + "\"status\": 200" + "}");
-            res.send(obj2);
-            //  res.send(obj2);
-        }
-        catch (e) {
-            console.log(e);
+        else {
             res.send(JSON.parse('{\"status\" : 404}'));
         }
-    });
+    }
+    else {
+        res.send(JSON.parse('{\"status\" : 404}'));
+    }
 });
 module.exports = router;
