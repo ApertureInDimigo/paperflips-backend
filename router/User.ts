5cf5de5a93d9e48ca7ddee257ec0e8027d3e61fa
WhiteB <whiteblock0715@gmail.com>
2020-08-15 12:03:25 +0900

    Update User.ts

--- router/User.ts
+++ router/User.ts
@@ -39,22 +39,22 @@ function checkconnect() {
 ///////////////모든 유저 정보를 가져옴
 router.get('/users', (req:any, res:any) => {
   checkconnect();
-  let token = req.headers.cookie;
+  let token = req.headers.cookie;  //쿠키 가져오기 
 
-  if(!isUndefined(token)) {
+  if(!isUndefined(token)) { //undefined가 아닐때..
 
   try {
-  let decode = jwt.verify(token.substring(5, token.length), secretObj.secret);
-  let isAdmin:boolean = decode.admin;
+  let decode = jwt.verify(token.substring(5, token.length), secretObj.secret); //토큰 검증
+  let isAdmin:boolean = decode.admin; //관리자 여부
 
-  if(isAdmin) {
+  if(isAdmin) { //관리자 일때.. 정상 프로세스
   connection.query('SELECT id, name, password, intro, favorite, deleted_day from Users', (error:any, rows:any) => {
     if (error) console.log(error);
     
     let rowstr:string = JSON.stringify(rows);
     res.send(JSON.parse(`[${rowstr.substring(1 , rowstr.length-1)}]`));
   });
-}else {
+}else { //관리자 일때.. 권한 없음 
   res.send(stat.get(403))
 } 
   }catch (e) {
@@ -70,41 +70,36 @@ router.get('/users', (req:any, res:any) => {
 
 /////////////// 회원 가입 
    router.post('/Adduser',(req:any, res:any) => {
+
+    //////////////////////id, password, name 필드 
     let id:string = req.body.id;
     let pwd:string = req.body.password;
     let name:string = req.body.name;
     
+    ///////////정규식 체크(SQL Injection 방지)
     if(check_id(id) && check_pwd(pwd) && check_name(name) ) {
     
-    
+      //32바이트의 랜덤 문자열 생성(salt)
       crypto.randomBytes(32, (err:any, buf:Buffer) => {
-        
+        //salt를 이용한 pwd 암호화
          crypto.pbkdf2(pwd , buf.toString('base64'), 126117, 64, 'sha512', (err:any, key:any) => {
-           let pwd:string = key.toString('base64');
-           let salt:string = buf.toString('base64');
+           let pwd:string = key.toString('base64'); //암호화한 pwd 
+           let salt:string = buf.toString('base64'); //랜덤 문자열 salt
 
            log('POST USERS id : ' + id +' pwd : ' + pwd + ' name : '+ name, 'default');
 
     let data = [id, pwd, name, salt];
     let sql:string = 'INSERT INTO Users (id, password, name, salt) VALUES(?, ?, ?, ?)';
 
-  checkconnect();
+  checkconnect(); //연결 설정 
  connection.query(sql, data,(err:any, results:any) => {
    if(err) res.send(stat.get(404))
-
-
    res.send(stat.get(200))
- });
+     });
 
 
          });
        });
-  
-   
-   
-   
-  
-
     }
     else {
       res.send(stat.get(404))
@@ -177,7 +172,6 @@ router.get('/check', (req:any, res:any) => {
             let token = jwt.sign(
               {
                 id : req.body.id,
-                password : key.toString('base64'),
                 admin : isAdmin(req.body.id),
               },
               secretObj.secret,
