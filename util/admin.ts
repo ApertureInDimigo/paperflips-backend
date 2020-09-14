a80ff162953c71f6c3b50a7aafc412e9c4b5dcd7
WhiteB <whiteblock0715@gmail.com>
2020-09-14 21:48:52 +0900

    어드민 리스트 추가

--- util/admin.ts
+++ util/admin.ts
@@ -1,6 +1,10 @@
 export function isAdmin(input:string) {
     let adminList:Array<string> = new Array<string>(
-        "minsoo0715"
+        "minsoo0715",
+        "ruadl034",
+        "qeqwqeq1",
+        "mj2044001",
+        "hodudragon0405"
         );
     
     if(adminList.indexOf(input) == -1) return false;
