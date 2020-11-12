b5ec53d808458064aba61c330f038311ff4319cc
root <root@paperflips.com>
2020-11-12 08:12:39 +0000

    update server.ts

--- config/database.ts
+++ config/database.ts
@@ -2,9 +2,9 @@
  import * as mysql from 'mysql';
  
  export let dbconfig:mysql.ConnectionConfig = {
-    host     : 'us-cdbr-east-02.cleardb.com',
-    user     : 'b5c72055d07cd3',
-    password : 'afca248a',
-    database : 'heroku_4ca6dadd6911bbc',
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Aperturecs1',
+    database : 'paperflips',
     multipleStatements: true
   };
