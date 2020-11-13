93c7166b489629f36654a6f6ae66740a6db86aca
root <root@paperflips.com>
2020-11-13 06:43:10 +0000

    node_env setting

--- server.ts
+++ server.ts
@@ -8,6 +8,7 @@ let fs = require('fs');
 let favicon = require('serve-favicon')
 let path = require('path')
 let helmet = require('helmet');
+let compression = require('compression');
 
 import {Request, Response, NextFunction, Router} from 'express'
 import {logs_, logs_http} from './Bot/botplay'
@@ -15,6 +16,7 @@ import {logs_, logs_http} from './Bot/botplay'
 
 
 let app = express();
+app.use(compression());
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -22,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(favicon(path.join(__dirname, 'favicon', 'favicon.ico')));
 
 app.use(function(req:Request,res:Response,next:NextFunction) { 
-  if(!req.secure){ res.redirect(307, "https://paperflips.com"+req.url); return; }else{ 
+  if(!req.secure && process.env.NODE_ENV == "production"){ res.redirect(307, "https://paperflips.com"+req.url); return; }else{ 
     _request.get({
       url: `http://ip-api.com/json/${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`
     }, function(error:any, response:any, body:any) {
@@ -48,7 +50,7 @@ app.use(function(req:Request,res:Response,next:NextFunction) {
    }  
 })
 
-
+console.log(process.env.NODE_ENV);
 let router:Router = require('./router/main')(app);
 
 
@@ -56,11 +58,12 @@ app.set('views', __dirname + '/public');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-let server = app.listen(80 ,'141.164.50.191' ,function(){
+
+if(process.env.NODE_ENV == "production") {
+
+  let server = app.listen(80 ,'141.164.50.191' ,function(){
     console.log("Express server has started on port 80");
 })
-
-
 const options = {
   ca: fs.readFileSync('ssl-key/paperflips_com.ca-bundle'),
   cert: fs.readFileSync('ssl-key/paperflips_com.crt'),
@@ -69,6 +72,15 @@ const options = {
 https.createServer(options, app).listen(443,'141.164.50.191', function() {
         console.log("Express server has started on port 443");
 });
+
+}else if(process.env.NODE_ENV == "development") {
+    let server = app.listen(8000, function() {
+      console.log("Express server has started on port 8000")
+      })
+}
+
+
+
 
 
 app.use(express.static('public'));
