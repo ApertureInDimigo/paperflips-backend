525f1254bf4bc6984fa3424a75b96e5c7c8f46d0
WhiteB <whiteblock0715@gmail.com>
2020-11-17 11:39:13 +0900

    렌더 방식에서 파일 보내는 방식으로

--- MiddleWare/etc.ts
+++ MiddleWare/etc.ts
@@ -1,7 +1,9 @@
 import {Request, Response} from 'express';
+import path from 'path'
 
 export let privacy = (req:Request, res:Response) => {
-    res.render('privacy.html')
+    res.sendFile( path.resolve("public", "privacy.html"));
+    //res.render('privacy.html'/..)
 }
 
 export let robot = (req:Request, res:Response) => {
