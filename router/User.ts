4e593c901b9eee713849539e0df6446d50c7e663
WhiteB <whiteblock0715@gmail.com>
2020-11-17 11:38:51 +0900

    라우터 주석

--- router/User.ts
+++ router/User.ts
@@ -1,21 +1,20 @@
-
 import * as express from 'express'
 
-let router = express.Router();
+let router:express.Router = express.Router();
 
 import {auth} from '../Controller/authetication'
 import {user} from '../MiddleWare/user'
 import {bot_check} from '../MiddleWare/botcheck'
 
-router.get(`/users`,user.connectCheck, auth(true), user.getAll) //모든 유저 정보를 가져옴  admin 권한
+router.get(`/users`, user.connectCheck,auth(true), user.getAll) //모든 유저 정보를 가져옴  admin 권한
 
 router.get('/GetCollection', user.connectCheck,auth(false), user.getCollection); //아이디 체크
 
 router.post(`/AddCollection/:cId`, user.connectCheck, auth(false), user.addCollection) //컬렉션 추가
 
-router.post('/Adduser', user.connectCheck, user.add)
+router.post('/Adduser', user.connectCheck, user.add) //회원 가입
 
-router.get('/GetMyInfo',user.connectCheck, auth(false), user.getMyInfo)
+router.get('/GetMyInfo',user.connectCheck, auth(false), user.getMyInfo) //자신의 정보 얻어오기 
 
 router.get('/check', bot_check)  //디코 봇 서버 체크 
 
@@ -25,6 +24,6 @@ router.get('/check', bot_check)  //디코 봇 서버 체크
 
 router.get('/myRoom', user.connectCheck, auth(false), user.getMyRoom) //방 가져오기
 
-router.put(`/RoomDataChange/:seq`, auth(false), user.updateRoom) //방 정보 업데이트
+router.put(`/RoomDataChange/:seq`, user.connectCheck, auth(false), user.updateRoom) //방 정보 업데이트
 
  module.exports = router;
--- router/recipe.ts
+++ router/recipe.ts
@@ -2,19 +2,19 @@ import * as express from 'express';
 import {recipe} from '../MiddleWare/recipe'
 import {upload} from '../Image_Server/storage_handler'
 import {auth} from '../Controller/authetication'
+
 let router:express.Router = express.Router();
 
-//////////////////////////////레시피 데이터 
-router.get(`/data/:seq`, recipe.connectCheck,recipe.get);
-//////////// 운영자 권한 
-router.post(`/Upload`,recipe.connectCheck,auth(true),upload.single(`img`), recipe.upload) //파라미터로
+router.get(`/data/:seq`, recipe.connectCheck,recipe.get); //레시피 데이터 
 
-router.get(`/Search`, recipe.connectCheck,recipe.search);
+router.post(`/Upload`,recipe.connectCheck,auth(true),upload.single(`img`), recipe.upload) //레시피 업로드
+
+router.get(`/Search`, recipe.connectCheck,recipe.search); //검색
 
 router.get(`/AllData`, recipe.connectCheck, recipe.getAll)   //모든 레시피 데이터 가져오기 LIMIT 추가 예정.
   
-router.post(`/AddDetail/:recipeName`, recipe.connectCheck, auth(true), recipe.addDetail)
+router.post(`/AddDetail/:recipeName`, recipe.connectCheck, auth(true), recipe.addDetail) //상세 설명 추가
 
-router.get(`/GetDetail/:recipeName`, recipe.connectCheck, recipe.getDetail)
+router.get(`/GetDetail/:recipeName`, recipe.connectCheck, recipe.getDetail) //상세 설명 가져오기
 
  module.exports = router;
